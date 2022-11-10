f2a1eb50f1420c1a2c3c61319123c2a80da7da6e
RafaelC97 <rafael.castro97@hotmail.com>
2022-11-09 23:25:15 -0300

    Aula02

---
+++ aula02/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    //TIPOS PRIMITIVOS DE DADOS
+
+    int numerosInteiro = 1879; //numeros sem casas decimais
+    float numeroReal = 1.678897; //numeros com casas decimais
+    char caractere = '#'; //caracteres
+
+    int vetorDeInteiros[10] = {10, 20, 30, 40};
+    int vetorDeReais[10] = {1.5, 1.75, 2.0, 2.25, 2.5};
+    int vetorCaracteres['R', 'a', 'f', 'a', 'e', 'l'];
+
+    return 0;
+}
